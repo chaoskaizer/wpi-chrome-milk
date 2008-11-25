efbbfed778644e46302ba4865c1e69e138545dd4
Avice D <ck@animepaper.net>
2008-11-25 02:34:39 +0000

    fixes client-time-script, change "dawn tag" from *dn* to *dw*.

--- public/scripts/client-time.js
+++ public/scripts/client-time.js
@@ -1,1 +1,1 @@
-dt=new Date();tn=dt.getTime();dt.setTime(tn);th=dt.getHours();if(th>=20){td="nt"}else if(th>=17){td="dk"}else if(th>=9){td="dy"}else if(th>=5){td="dn"}else{td="nt"};
+dt=new Date();tn=dt.getTime();dt.setTime(tn);th=dt.getHours();if(th>=20){td="nt"}else if(th>=17){td="dk"}else if(th>=9){td="dy"}else if(th>=5){td="dw"}else{td="nt"};
